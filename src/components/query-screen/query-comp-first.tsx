c1706f00eb5d44e6506b1617a56ab92774bf7878
Oleg <akimba@gmail.com>
2022-11-04 13:47:18 +0300

    feat: Добавляет возможность отправки данных через RTK query

--- src/components/query-screen/query-comp-first.tsx
+++ src/components/query-screen/query-comp-first.tsx
@@ -1,7 +1,23 @@
-import { useLoadOffersQuery } from '../../redux-implements/rtk-query';
+import { useState, ChangeEvent, MouseEvent } from 'react';
+import { useAddOfferMutation, useLoadOffersQuery } from '../../redux-implements/rtk-query';
+import { Offer } from '../../types/data';
 
 function QueryComponentFirst(): JSX.Element {
+  const [ value, setValue ] = useState('');
   const { data } = useLoadOffersQuery('');
+  const [ addOffer ] = useAddOfferMutation();
+
+  const handleAddOffer = async (evt: MouseEvent) => {
+    evt.preventDefault();
+    const newOffer = {
+      title: value,
+      price: 2000,
+    } as Offer;
+    if (value) {
+      await addOffer(newOffer);
+      setValue('');
+    }
+  };
   return (
     <section className="query-component">
       <h2 className="query-component__title">query-based component 1</h2>
@@ -15,6 +31,17 @@ function QueryComponentFirst(): JSX.Element {
           </li>
         ))}
       </ul>
+      <form className="query-component__form">
+        <label>
+          Введите название:
+          <input className="query-component__input"
+            type="text"
+            name="text-input"
+            value={value}
+            onChange={(evt: ChangeEvent<HTMLInputElement>) => setValue(evt.target.value)} />
+        </label>
+        <button onClick={handleAddOffer}>Добавить в список</button>
+      </form>
     </section>
   );
 }
--- src/redux-implements/rtk-query.ts
+++ src/redux-implements/rtk-query.ts
@@ -6,10 +6,20 @@ import { ApiRoute, BASE_URL } from '../const';
 const offerApi = createApi({
   reducerPath: 'offer-data',
   baseQuery: fetchBaseQuery( {baseUrl: BASE_URL} ),
+  tagTypes: ['Offer'],
   endpoints: (build) => ({
     loadOffers: build.query<Offer[], string>({
       query: () => ApiRoute.Offers,
+      providesTags: (result) => ['Offer'],
     }),
+    addOffer: build.mutation<Offer, Offer>({
+      query: (offer) => ({
+        url: ApiRoute.Offers,
+        method: "POST",
+        body: offer,
+      }),
+      invalidatesTags: ['Offer'],
+    })
   }),
 });
 
@@ -20,4 +30,4 @@ export const store = configureStore({
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(offerApi.middleware),
 });
 
-export const { useLoadOffersQuery } = offerApi;
+export const { useLoadOffersQuery, useAddOfferMutation } = offerApi;
